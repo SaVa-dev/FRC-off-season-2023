f0333a4b65fe99367ce233e633f32cc8e7c18b0e
SaVa-dev <santymatusg@gmail.com>
2023-07-06 22:20:13 -0600

    Arcade drive robot (sin probar)

---
+++ Wigetta/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.subsystems.ChasisDrive;
+  
+
+public class Robot extends TimedRobot {
+  public final ChasisDrive chasisDrive = new ChasisDrive(
+    0, 1, 2, 3);
+  public PS4Controller controller = new PS4Controller(0);
+  
+  @Override
+  public void robotInit() {
+    
+  }
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void autonomousExit() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    chasisDrive.input(controller.getLeftY(), controller.getLeftX());
+  }
+
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
+}
---
+++ Wigetta/src/main/java/frc/robot/subsystems/ChasisDrive.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ChasisDrive extends SubsystemBase {
+  /** Creates a new ChasisDrive. */
+
+  Spark m_leftFront, m_rightFront, m_leftBack, m_rightBack;
+  MotorControllerGroup m_left, m_right;
+  DifferentialDrive driveTrain;
+
+  public ChasisDrive(int m_leftFront, int m_rightFront, int m_leftBack, int m_rightBack) {
+    this.m_leftBack = new Spark(m_leftBack);
+    this.m_rightBack = new Spark(m_rightBack);
+    this.m_leftFront = new Spark(m_leftFront);
+    this.m_rightFront = new Spark(m_rightFront);
+    this.m_left = new MotorControllerGroup(this.m_leftBack, this.m_leftFront);
+    this.m_right = new MotorControllerGroup(this.m_rightBack, this.m_rightFront);
+    this.driveTrain = new DifferentialDrive(m_left, m_right);
+    this.m_right.setInverted(true);
+  }
+
+  public void input(double yAxis, double zAxis) {
+    driveTrain.arcadeDrive(yAxis, zAxis);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
